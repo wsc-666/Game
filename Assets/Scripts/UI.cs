e05f659b52ee3d52ef2ab661bd37d1c40a10f2f5
wsc-666 <1916888365@qq.com>
2020-05-31 10:39:01 +0800

    UI
    游戏暂停，重新加载，排行榜，血条UI，

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public Button button;
+    public GameObject gamePanle;
+    private bool panleIsTrue = false;
+    private bool isPause = false;
+    public void OnBtnClick()
+    {
+
+        OpenPanel();
+
+    }
+
+    public void OnBtnStartClick()
+    {
+
+        Pause();
+
+    }
+    public void OnBtnLoadClick()
+    {
+
+        Load();
+
+    }
+    public void Load()
+    {
+
+        SceneManager.LoadScene(0);
+
+    }
+    public void Pause()
+    {
+        Time.timeScale = isPause ? 1 : 0;
+        isPause = !isPause;
+    }
+    public void OpenPanel()
+    {
+        if (!panleIsTrue)
+        {
+            gamePanle.SetActive(true);
+        }
+        else
+        {
+            gamePanle.SetActive(false);
+        }
+        panleIsTrue = !panleIsTrue;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
